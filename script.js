b3311a8e22bb682c84ca14f09402522979ee1a0b
ChristianGraziano <123503135+ChristianGraziano@users.noreply.github.com>
2023-04-15 00:35:59 +0200

    M2_D8--Prova valutata
    creazione di uh algoritmo che ricerca un determinato lavoro richiesto dall'utente.

---
+++ script.js
@@ -0,0 +1,170 @@
+/*
+PARTE 1: 
+Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
+Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
+Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
+L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
+Prendi queste tre inserzioni ad esempio:
+
+      job1:  location: "NY, US",     title: "java dev"
+      job2:  location: "Genoa, IT"   title: "web dev"
+      job3:  location: "US"      title: "dev"
+
+Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato solamente job1 e job3,
+in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
+
+REQUISITI:
+- il tuo algoritmo deve tornare i risultati nella seguente forma:
+{
+  result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
+  count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
+}
+
+- da ogni inserzione trovata, elimina i campi "description", "requirements", "benefits" e "company_profile" per semplificare il risultato
+
+- la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
+
+
+PARTE 2: 
+Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
+
+Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
+
+Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
+    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
+    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
+    SUGGERIMENTO: ti servira’ un ciclo for!
+
+*/
+
+// NON MODIFICARE QUESTO ARRAY!
+const jobs = [
+  { title: "Marketing Intern", location: "US, NY, New York" },
+  {
+    title: "Customer Service - Cloud Video Production",
+    location: "NZ, Auckland",
+  },
+  {
+    title: "Commissioning Machinery Assistant (CMA)",
+    location: "US, IA, Wever",
+  },
+  {
+    title: "Account Executive - Washington DC",
+    location: "US, DC, Washington",
+  },
+  { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+  { title: "Accounting Clerk", location: "US, MD," },
+  { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+  {
+    title: "Lead Guest Service Specialist",
+    location: "US, CA, San Francisco",
+  },
+  { title: "HP BSM SME", location: "US, FL, Pensacola" },
+  {
+    title: "Customer Service Associate - Part Time",
+    location: "US, AZ, Phoenix",
+  },
+  {
+    title: "ASP.net Developer Job opportunity at United States,New Jersey",
+    location: "US, NJ, Jersey City",
+  },
+  {
+    title: "Talent Sourcer (6 months fixed-term contract)",
+    location: "GB, LND, London",
+  },
+  {
+    title: "Applications Developer, Digital",
+    location: "US, CT, Stamford",
+  },
+  { title: "Installers", location: "US, FL, Orlando" },
+  { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+  {
+    title: "VP of Sales - Vault Dragon",
+    location: "SG, 01, Singapore",
+  },
+  { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+  {
+    title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+    location: "GB, SOS, Southend-on-Sea",
+  },
+  { title: "Visual Designer", location: "US, NY, New York" },
+  {
+    title: "Process Controls Engineer - DCS PLC MS Office - PA",
+    location: "US, PA, USA Northeast",
+  },
+  { title: "Marketing Assistant", location: "US, TX, Austin" },
+  { title: "Front End Developer", location: "NZ, N, Auckland" },
+  { title: "Engagement Manager", location: "AE," },
+  {
+    title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+    location: "US, CA, Carlsbad",
+  },
+  { title: "Customer Service", location: "GB, LND, London" },
+  { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+  { title: "Marketing Exec", location: "SG," },
+  {
+    title: "HAAD/DHA Licensed Doctors Opening in UAE",
+    location: "AE, AZ, Abudhabi",
+  },
+  {
+    title: "Talent Management Process Manager",
+    location: "US, MO, St. Louis",
+  },
+  { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+  {
+    title: "Customer Service Technical Specialist",
+    location: "US, MA, Waltham",
+  },
+  { title: "Software Applications Specialist", location: "US, KS," },
+  { title: "Craftsman Associate", location: "US, WA, Everett" },
+  { title: "Completion Engineer", location: "US, CA, San Ramon" },
+  { title: "I Want To Work At Karmarama", location: "GB, LND," },
+  {
+    title: "English Teacher Abroad",
+    location: "US, NY, Saint Bonaventure",
+  },
+]
+
+
+
+//Come prima cosa mi creo delle variabili d'appoggio che conterranno 
+let content = 0;
+let content2 = 1;
+
+//creo un array vuoto che conterrà il risultato di cio che l'utente cercherà nell'input dell'html
+//questo array conterra il risultato finale dell'intero evento/funzione.
+searchResults = [];
+
+let search =  (event) => {
+  
+  // Mi creo due variabili in cui vado a catturare i 2 input delll'html e il paragrafo dove sara immesso l'output dell'algoritmo per riportarlo a schermo.
+  let titleLow = document.getElementById("title");
+  let locationLow = document.getElementById("location");
+  let p = document.querySelector("p")
+  
+  // creo un ciclo di for che va ad iterare ed ispezionare tutto l'array principali che contiene gli annunci di lavoro
+  for (let i = 0; i < jobs.length; i++) {
+    
+    //con questa if vado a chiedere: se nell'arrai che stai iterando, sia title che location (entrambe in LowerCase) includono come valore cio che e determinato dall'utente nei due input,
+    //aggiungimi all'arrai principale (searchResult) l'oggetto che corrisponde alla ricerca (jobs[i])
+    if (jobs[i].title.toLowerCase().includes(titleLow.value) && jobs[i].location.toLowerCase().includes(locationLow.value)) {
+      searchResults.push(jobs[i]);
+      
+      //innietto due stringhe dentro al paragrafo utilizzato come output separate da (\n che indica una nuova riga)
+      p.innerText += ("Job "+ content2 + ": " + searchResults[content].title + "\nlocation:  " + searchResults[content].location + "\n")
+      
+      console.log("Job "+ content2 + ": " + searchResults[content].title + "\nlocation:  " + searchResults[content].location);
+      
+      content ++;
+      content2 ++
+      }
+    }
+            return searchResults;
+  }
+            
+   // creo una funzione che assegno al paragrafo cioè il nostro uotput in cui innietto una stringa fuota per pulire lo schermo ed effettuare una nuova ricarca con degli altri parametri.
+    let rubber = () => {
+    let p = document.querySelector("p")
+    p.innerHTML = "";
+  }
+
